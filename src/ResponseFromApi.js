b488adf7443ce73f3daa2c2ed99918edbb081590
teitonny <tonnytei4@gmail.com>
2023-02-10 19:10:42 +0300

    removed linter errors

--- src/ResponseFromApi.js
+++ src/ResponseFromApi.js
@@ -5,7 +5,6 @@ const getApiResponse = async (id, likeCount) => {
   const response = await fetch(url);
 
   const result = await response.json();
-  console.log(result);
   const likeData = result.find((item) => item.item_id === id);
   if (likeData) {
     likeCount.textContent = likeData.likes;
