90f027a7824f6f0fa08777f6080af2383975bcd1
Tonny Tei <tonnytei4@gmail.com>
2023-02-11 18:41:59 +0300

    removed unnecessary code from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -42,9 +42,6 @@ const config = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        // generator: {
-        //   filename: 'images/[name][ext]',
-        // },
       },
 
       // Add your rules for custom modules here
